ff74714a5c962c1beadaeff0b204d7f4d94345a3
AminMasoumi <aminrezamasoumi72@gmail.com>
2025-08-05 20:40:02 +0330

    revert

--- Hospital/Hospital/Person.cs
+++ Hospital/Hospital/Person.cs
@@ -12,6 +12,7 @@ namespace Hospital
         public int Id { get; set; }
         public string FirstName { get; set; }
         public string LastName { get; set; }
+        public string Phone { get; set; }
         public string LastName2 { get; set; }
 
     }
