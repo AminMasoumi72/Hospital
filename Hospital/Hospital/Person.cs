6752652d0fb8c16e71c54b57b91e3d3bfec433cc
AminMasoumi <aminrezamasoumi72@gmail.com>
2025-08-05 20:12:37 +0330

    remove Phone

--- Hospital/Hospital/Person.cs
+++ Hospital/Hospital/Person.cs
@@ -12,6 +12,5 @@ namespace Hospital
         public int Id { get; set; }
         public string FirstName { get; set; }
         public string LastName { get; set; }
-        public string Phone { get; set; }
     }
 }
