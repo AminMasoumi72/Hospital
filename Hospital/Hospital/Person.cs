f3586c6a1e23245278a8c31b16034d0dd6226086
AminMasoumi <aminrezamasoumi72@gmail.com>
2025-08-05 19:49:19 +0330

    chang111

--- Hospital/Hospital/Person.cs
+++ Hospital/Hospital/Person.cs
@@ -1,12 +1,17 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Permissions;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace Hospital
 {
     internal class Person
-    {
+    {       
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Phone { get; set; }
     }
 }
